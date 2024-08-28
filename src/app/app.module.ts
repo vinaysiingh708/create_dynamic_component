cf42c50da9d3887c002d3cdc83ebf8335e6caece
vinay singh <vinaysingh708@yahoo.com>
2024-08-28 19:40:13 +0530

    commit main code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { ConfirmDeleteComponent } from './users/confirm-delete/confirm-delete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UsersComponent,
+    ConfirmDeleteComponent
   ],
   imports: [
     BrowserModule,
